349a167107b439536964e704f250ed9021d1c485
TechWiz-3 <programmingplus1@gmail.com>
2021-12-30 09:27:11 +1100

    reference types demo

--- lessonOne.java
+++ lessonOne.java
@@ -1,12 +1,18 @@
-import java.util.Date;
-
-import javax.sound.sampled.SourceDataLine;
+import java.awt.*;
 
 public class lessonOne {
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);
+        //this demonstrates how primitive type
+        //variables are independent of eachother
+        
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
         
     }
 }
