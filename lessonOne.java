07aea0c5c9bc4fb036be6fd7faa1fab3ad1175d1
TechWiz-3 <programmingplus1@gmail.com>
2021-12-29 21:16:37 +1100

    primitive types

--- lessonOne.java
+++ lessonOne.java
@@ -1,7 +1,12 @@
 public class lessonOne {
     public static void main(String[] args) {
-        int myAge = 20;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte age = 30;
+        int viewsCountA = 123_456_789;
+        long viewsCountB = 3_123_456_489L;
+        double fraction = 1.2;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(viewsCountA);
     }
 }
