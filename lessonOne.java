60dcdae14533d3ce28b02cd74084a8b21ba7e266
TechWiz-3 <programmingplus1@gmail.com>
2021-12-29 21:25:46 +1100

    reference types

--- lessonOne.java
+++ lessonOne.java
@@ -1,12 +1,12 @@
+import java.util.Date;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class lessonOne {
     public static void main(String[] args) {
         byte age = 30;
-        int viewsCountA = 123_456_789;
-        long viewsCountB = 3_123_456_489L;
-        double fraction = 1.2;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-        System.out.println(viewsCountA);
+        Date now = new Date();
+        System.out.println(now);
+        
     }
 }
